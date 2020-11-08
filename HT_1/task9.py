ff60929d1d4ebccdd7c9122dcb984558401825aa
Ivan Masiuk <45178554+ivan-masiuk@users.noreply.github.com>
2020-11-08 23:18:24 +0200

    Add files via upload

---
+++ HT_1/task9.py
@@ -0,0 +1,7 @@
+# Write a script to remove an empty tuple(s) from a list of tuples.
+#         Sample data: [(), (), ('',), ('a', 'b'), ('a', 'b', 'c'), ('d')]
+#         Expected output: [('',), ('a', 'b'), ('a', 'b', 'c'), 'd']
+
+filter(None, a)
+
+print(a)
