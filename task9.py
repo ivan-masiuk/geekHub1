088a793f9353e446bc09b2a8e2954fa646bc5675
Ivan Masiuk <45178554+ivan-masiuk@users.noreply.github.com>
2020-11-08 23:22:31 +0200

    Delete task9.py

--- task9.py
+++
@@ -1,7 +0,0 @@
-# Write a script to remove an empty tuple(s) from a list of tuples.
-#         Sample data: [(), (), ('',), ('a', 'b'), ('a', 'b', 'c'), ('d')]
-#         Expected output: [('',), ('a', 'b'), ('a', 'b', 'c'), 'd']
-
-filter(None, a)
-
-print(a)
