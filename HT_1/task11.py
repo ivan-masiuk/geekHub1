bddbd6390d8af2e8587bcbd24f76827c60dc9157
Ivan Masiuk <45178554+ivan-masiuk@users.noreply.github.com>
2020-11-09 14:29:07 +0200

    Update task11.py

--- HT_1/task11.py
+++ HT_1/task11.py
@@ -1,9 +1,9 @@
 # Write a script to remove duplicates from Dictionary.
-
-result = {}
+dic1 = {} #початковий словник
+result = {} #словник без duplicates
 
 for key,value in dic1.items():
     if value not in result.values():
         result[key] = value
 
-print result
+print (result)
