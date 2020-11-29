c5c6c752dc6ad4a7ca531c4e23f87f10ca707372
Ivan Masiuk <45178554+ivan-masiuk@users.noreply.github.com>
2020-11-29 21:17:26 +0200

    Add files via upload

--- HT_6/main.py
+++ HT_6/main.py
@@ -37,7 +37,7 @@ def restart():
         start_transactions = []
         json.dump(start_transactions, write_file)
         write_file.close()
-    # start()
+    start()
 
 # add new user function
 def addUser():
@@ -86,7 +86,7 @@ def autoriationUser():
         if user_name in users_dict:
             user_password = str(input ('Print your password: '))
             counter = 2
-            for _ in range(2):
+            for _ in range(3):
                 if users_dict[user_name] == user_password:
                     print(f'\n{user_name}, welcome!')
                     status_user = True
@@ -94,9 +94,7 @@ def autoriationUser():
                 else:
                     print(f'Incorrect password! Try again, you have {counter} attempt more')
                     counter -= 1             
-                    user_password = str(input ('Print your password again: '))
-                if counter == 0:
-                    print('Sorry, you have 0 attempt.')    
+                    user_password = str(input ('Print your password again: '))                 
         else:
             print(f'Any users with {user_name} name.')
     return(status_user, user_name)
@@ -121,7 +119,7 @@ def getMoney(user_name):
         counter100 = desire//100
         desire -= 100 * counter100
         counter50 = desire//50
-        transaction_text = f'GOT maney: {desire}'
+        transaction_text = f'GOT maney: {desire1}'
         # balance.data update
         write_file = open(balance_data, 'w')
         json.dump(new_users_balace, write_file)
@@ -135,7 +133,7 @@ def getMoney(user_name):
         write_file = open(transaction_data, 'w')
         json.dump(transactions_list, write_file)
         write_file.close()
-        print(f'Get money: {desire}UAH!\n Banknotes: 500 UAH x {counter500}, 200 UAH x {counter200}, 100 UAH x {counter100}, 50 UAH x {counter50}')
+        print(f'Get money: {desire1} UAH!\nBanknotes: 500 UAH x {counter500}, 200 UAH x {counter200}, 100 UAH x {counter100}, 50 UAH x {counter50}')
     else:
         print('Not enought money!')
     start()
@@ -164,7 +162,7 @@ def topUpBalance(user_name):
     transactions_list.append(transaction_text)
     json.dump(transactions_list, write_file)
     write_file.close()
-    print(f'You have replenished the balance by {desire}UAH\n')
+    print(f'You have replenished the balance by {desire} UAH\n')
     start()
 
 # check balance function
@@ -191,7 +189,6 @@ def topUpBanknotes():
     start()
 
 # start function
-restart()
 a = autoriationUser()
 status, name = a[0], a[1]
 def start(user_name = name, user_status = status):
@@ -224,41 +221,3 @@ def start(user_name = name, user_status = status):
                 start()
 
 start()
-
-# restart function
-def restart():
-    # remove all .data files 
-    del_file_list = [ f for f in os.listdir(os.path.dirname(os.path.abspath(__file__))) if f.endswith(".data") ]
-    for f in del_file_list:
-        os.remove(os.path.join(os.path.dirname(os.path.abspath(__file__)), f))
-    # create users.data
-    users = {
-        'Ivan': '1',
-        'Misha': '2',
-        'admin':'admin'
-    }
-    users_data = os.path.join(os.path.dirname(os.path.abspath(__file__)),'users.data')
-    write_file = open(users_data, 'w')
-    json.dump(users, write_file)
-    write_file.close()
-    # add banknotes.data
-    banknotes_data = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'banknotes.data')
-    write_file = open(banknotes_data, 'w')
-    banknotes = {'500':100, '200':100, '100':100, '50':100}
-    json.dump(banknotes, write_file)
-    write_file.close()
-    # create <name>_balance.data
-    for user in users:
-        balance_data = os.path.join(os.path.dirname(os.path.abspath(__file__)), f'{user}_balance.data')
-        write_file = open(balance_data, 'w')
-        start_balance = 1000
-        json.dump(start_balance, write_file)
-        write_file.close()
-    # create <name>_transactions.data
-    for user in users:
-        transactions_data = os.path.join(os.path.dirname(os.path.abspath(__file__)), f'{user}_transactions.data')
-        write_file = open(transactions_data, 'w')
-        start_transactions = []
-        json.dump(start_transactions, write_file)
-        write_file.close()
-    start()
